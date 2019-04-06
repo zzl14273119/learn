6337b4136e5c5d6378c4f7fe146a10fb7c26d1e7
zzl14273119 <1415169404@qq.com>
2019-04-06 19:42:12 +0800

    Signed-off-by: zzl14273119 <1415169404@qq.com>

---
+++ src/main/java/com/zzl/HelloApp.java
@@ -0,0 +1,16 @@
+package com.zzl;
+
+/**
+ * @ClassName HelloApp
+ * @Description TODO
+ * @Author zhangzl
+ * @Date 2019/3/30 13:49
+ * @Version 1.0
+ **/
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!   ");
+    }
+
+}
