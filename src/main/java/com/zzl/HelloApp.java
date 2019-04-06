6da464f5b2c751d509854b41dd753388729974c8
zzl14273119 <1415169404@qq.com>
2019-04-06 19:53:40 +0800

    Update HelloApp.java

--- src/main/java/com/zzl/HelloApp.java
+++ src/main/java/com/zzl/HelloApp.java
@@ -10,7 +10,7 @@ package com.zzl;
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World, zzl!");
     }
 
 }
